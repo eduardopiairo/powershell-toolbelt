4c1611af917cd5a8580e55236780db8df8bb2736
edpiairo-ux360 <eduardopiairo@gmail.com>
2018-07-19 01:16:47 +0100

    basics update

--- basics.ps1
+++ basics.ps1
@@ -1,23 +1,34 @@
-#Get modules in the system
+###** The BASICS 
+
+# Get modules in the system
 Get-Module -All
 
-#Get specific module
+
+# Get specific module
 Get-Module -ListAvailable -Name dbatools
 Get-Module -ListAvailable -Name Azure
 
+
+# Unnistall a module
 Uninstall-Module -Name Pester -AllVersions
 
 
-### Install Modules
+# Install Modules
 
-# Pester
+## Pester
 Install-Module -Name Pester -Force -SkipPublisherCheck
 
+# Get available functions
 Get-Command -Module pester -CommandType Function
 
-#dbatools 
+## dbatools 
 Install-Module dbatools
 
+Update-Module dbatools
+
+Import-Module dbatools
+
+# Get available functions
 Get-Command -Module dbatools -CommandType Function
 
 
